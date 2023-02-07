7bcc7bd6afb9ccfe4c8dcbdcc3e226ab857b6540
deveshd11221 <47088431+deveshd11221@users.noreply.github.com>
2023-02-07 11:38:01 -0500

    Add files via upload
    testing a new service worker file. Hopefully this might change things?

--- sw.js
+++ sw.js
@@ -1,20 +1,84 @@
-// On install - caching the application shell
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      // cache any static files that make up the application shell
-      return cache.add('index.html');
-    })
-  );
-});
+// Ensure that the browser supports the service worker API
+if (navigator.serviceWorker) {
+// Start registration process on every page load
+window.addEventListener('load', () => {
+	navigator.serviceWorker
+		// The register function takes as argument
+		// the file path to the worker's file
+		.register('/service_worker.js')
+		// Gives us registration object
+		.then(reg => console.log('Service Worker Registered'))
+		.catch(swErr => console.log(
+				`Service Worker Installation Error: ${swErr}}`));
+	});
+}
 
-// On network request
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    // Try the cache
-    caches.match(event.request).then(function(response) {
-      //If response found return it, else fetch again
-      return response || fetch(event.request);
-    })
-  );
+var cacheName = 'geeks-cache-v1';
+var cacheAssets = [
+	'index.html',
+	'style.css',
+
+];
+
+// Call install Event
+self.addEventListener('install', e => {
+	// Wait until promise is finished
+	e.waitUntil(
+		caches.open(cacheName)
+		.then(cache => {
+			console.log(`Service Worker: Caching Files: ${cache}`);
+			cache.addAll(cacheAssets)
+				// When everything is set
+				.then(() => self.skipWaiting())
+		})
+	);
+})
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+	console.log('Service Worker: Activated');
+	// Clean up old caches by looping through all of the
+	// caches and deleting any old caches or caches that
+	// are not defined in the list
+	e.waitUntil(
+		caches.keys().then(cacheNames => {
+			return Promise.all(
+				cacheNames.map(
+					cache => {
+						if (cache !== cacheName) {
+							console.log('Service Worker: Clearing Old Cache');
+							return caches.delete(cache);
+						}
+					}
+				)
+			)
+		})
+	);
+})
+
+var cacheName = 'geeks-cache-v1';
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+	console.log('Service Worker: Fetching');
+	e.respondWith(
+		fetch(e.request)
+		.then(res => {
+			// The response is a stream and in order the browser
+			// to consume the response and in the same time the
+			// cache consuming the response it needs to be
+			// cloned in order to have two streams.
+			const resClone = res.clone();
+			// Open cache
+			caches.open(cacheName)
+				.then(cache => {
+					// Add response to cache
+					cache.put(e.request, resClone);
+				});
+			return res;
+		}).catch(
+			err => caches.match(e.request)
+			.then(res => res)
+		)
+	);
 });
